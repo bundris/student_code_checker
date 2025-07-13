9cf445686f4faefb8923e5e429e7f44fe8dbe8c1
Boris Karpov <skethime@gmail.com>
2025-07-13 16:04:54 +0200

    initial commit

---
+++ src/string_utils.py
@@ -0,0 +1,14 @@
+def reverse_string(s):
+    return s[::-1]
+
+def capitalize_words(s):
+    return ' '.join(word.capitalize() for word in s.split())
+
+def count_vowels(s):
+    #vowels = 'euioaEUIOA'
+    vowels = 'aui'
+    return sum(1 for char in s if char in vowels)
+
+def is_palindrome(s):
+    normalized = ''.join(c for c in s if c.isalnum())
+    return normalized == normalized[::-1]
